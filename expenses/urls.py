7f3557c93812675573936cf5525d406b39bdee45
Mihkel-Kritsmann <mihkelkritsmann@gmail.com>
2025-04-06 16:53:39 +0300

    uncommited changes to be committed

--- expenses/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'expenses'  
-
-urlpatterns = [
-    path('', views.IndexView.as_view()),
-    path('<int:pk>', views.ExpenseDetailView.as_view(), name='expenses'),
-]
--- tracker/views.py
+++ tracker/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
-from .models import Expense
+from .models import Expense, Income, Category
 from django.http import HttpResponse
 
 # Create your views here.
 class IndexView(ListView):
-    model = Expense
+    #model = Expense
     queryset = Expense.objects.all()
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['expenses'] = Expense.objects.all()
+        context['incomes'] = Income.objects.all()
+        context['categorys'] = Category.objects.all()
         return context
